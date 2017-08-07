d1df64a506e587897bee8fda29c97fc9f72f7551
maylaje <30810966+maylaje@users.noreply.github.com>
2017-08-07 13:18:14 -0700

    Add files via upload

---
+++ js/Riot.js
@@ -0,0 +1,13 @@
+var pass = document.getElementById("pass");
+var fail = document.getElementById("fail");
+
+button.onclick = function function_name(argument) {
+	// change buon color to green for pass, red for fail
+	if (pass.className = "") {
+		//change green
+		pass.className = "pass"
+	} 
+	else{
+		//
+	}
+}
